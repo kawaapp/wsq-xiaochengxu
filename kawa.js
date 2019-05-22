a8ce3a37a7e4a23a30007ea72feb22e4fd068078
ntop <ntop.liu@gmail.com>
2019-05-22 22:45:49 +0800

    new theme: blue

--- kawa.js
+++ kawa.js
@@ -1,7 +1,14 @@
+const theme_default = {
+  Image: '/res/default',
+  MainColor: '#20ACAB',
+}
+
+const theme_blue = {
+  Image: '/res/blue',
+  MainColor: '#1890ff',
+}
+
 module.exports = {
   AppKey: "J3PDS76SH6JASQHX2QAY5VQ3QINXIYLEMVIREFADB7Z2BHCWC3VA====",
-  Theme: {
-    Image: '/res/img',
-    MainColor: '#20ACAB',
-  }
+  Theme: theme_blue,
 }
