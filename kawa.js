97735a669228d413a9081a42780e63df49793742
ntop <ntop.liu@gmail.com>
2019-05-29 22:21:17 +0800

    new theme --- cyan

--- kawa.js
+++ kawa.js
@@ -8,7 +8,12 @@ const theme_blue = {
   MainColor: '#1890ff',
 }
 
+const theme_cyan = {
+  Image: '/res/cyan',
+  MainColor: '#20ACAB',
+}
+
 module.exports = {
   AppKey: "J3PDS76SH6JASQHX2QAY5VQ3QINXIYLEMVIREFADB7Z2BHCWC3VA====",
-  Theme: theme_default,
+  Theme: theme_cyan,
 }
