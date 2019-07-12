9e9434190cb410f6ad1a56d61d5a2acc88457834
ntop <ntop.liu@gmail.com>
2019-07-12 13:45:02 +0800

    ui: user exp screen

--- pages/exp/exp.js
+++ pages/exp/exp.js
@@ -1,11 +1,14 @@
-// pages/exp/exp.js
+// pages/signin/signin.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    user: {
+      avatar: 'https://gw.alipayobjects.com/zos/antfincdn/XAosXuNZyF/BiazfanxmamNRoxxVxka.png',
+      grade: 124,
+    },
   },
 
   /**
@@ -16,51 +19,9 @@ Page({
   },
 
   /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
 
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
