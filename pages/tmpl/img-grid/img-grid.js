954f472e015b06d824ce5008487f8ff1c587ac0d
ntop <ntop.liu@gmail.com>
2019-07-20 22:10:27 +0800

    comps:add image-grid component

---
+++ pages/tmpl/img-grid/img-grid.js
@@ -0,0 +1,35 @@
+// pages/tmpl/img-grid/img-grid.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    images: [
+      "https://images.kawaapp.com/img_bjkk50cdbfdqfm68t540.jpg",
+      // "https://images.kawaapp.com/img_bkbiucsdbfdqfm68targ.jpg",
+      // "https://images.kawaapp.com/img_bkbiucsdbfdqfm68targ.jpg",
+
+      // "https://images.kawaapp.com/img_bkbiucsdbfdqfm68targ.jpg",
+      // "https://images.kawaapp.com/img_bkbiucsdbfdqfm68targ.jpg",
+      // "https://images.kawaapp.com/img_bkbiucsdbfdqfm68targ.jpg",
+
+      // "https://images.kawaapp.com/img_bkbiucsdbfdqfm68targ.jpg",
+      // "https://images.kawaapp.com/img_bkbiucsdbfdqfm68targ.jpg",
+      // "https://images.kawaapp.com/img_bkbiucsdbfdqfm68targ.jpg"
+    ],
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
