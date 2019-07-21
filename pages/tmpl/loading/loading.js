1602755d744bc015f493268c0dca048a45aca2b3
ntop <ntop.liu@gmail.com>
2019-07-21 10:51:35 +0800

    ui:add loading component

---
+++ pages/tmpl/loading/loading.js
@@ -0,0 +1,26 @@
+// pages/tmpl/loading/loading.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    loader: {
+      type: Object,
+      value: {},
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
