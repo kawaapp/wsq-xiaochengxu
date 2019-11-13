9327f51c07f6d29acd77b17a825681a58f6f7c80
ntop <ntop.liu@gmail.com>
2019-11-13 20:04:52 +0800

    add component kw-like

---
+++ pages/tmpl/like/kw-like.js
@@ -0,0 +1,38 @@
+const kawa = require('../../../kawa.js')
+
+// pages/tmpl/like/kw-like.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    favored: {
+      type: Number,
+      value: 0,
+    },
+    favors: {
+      type: Number,
+      value: 0,
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    theme: {
+      images: kawa.Theme.Image,
+      favorColor: kawa.Theme.FavorColor || kawa.Theme.MainColor,
+    }
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onClick: function() {
+      console.log("click favor icon...")
+      this.triggerEvent('onClick')
+    }
+  }
+})
