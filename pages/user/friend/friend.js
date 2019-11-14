5bf6e3ca2b91a39434ef1c27e2248dfdd3deaf41
ntop <ntop.liu@gmail.com>
2019-11-14 18:38:44 +0800

    follow/unfollow works

--- pages/user/friend/friend.js
+++ pages/user/friend/friend.js
@@ -15,63 +15,87 @@ Page({
     followings: [],
     pageNumber: 1,
     pageSize: 20,
+    loader: {
+      ing: false, more: true,
+    }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    fetchFollowerList(this)
+    fetchFollowingList(this)
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    const { tab } = this.data
-    if (tab.current == 0) {
-      fetchFollowerList(this)
-    } else {
-      fetchFollowingList(this)
-    }
+    this.firstLoad()
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    const { tab } = this.data
-    if (tab.current == 0) {
-      fetchMoreFollower(this)
-    } else {
+    const { tabCurrent } = this.data
+    if (tabCurrent == 0) {
       fetchMoreFollowing(this)
+    } else {
+      fetchMoreFollower(this)
     }
   },
 
   onTabChanged: function(e) {
-    this.setData({ tabCurrent: e.detail })
+    this.setData({ tabCurrent: e.detail }, () => {
+      this.firstLoad()
+    })
+  },
+
+  firstLoad: function(e) {
+    const { tabCurrent } = this.data
+    if (tabCurrent == 0) {
+      fetchFollowingList(this)
+    } else {
+      fetchFollowerList(this)
+    }
   },
 
   clickUnfollow: function(e) {
     var idx = e.currentTarget.dataset.idx
-    var user = view.data.followers[idx]
-    api.unfollow(user.id).then( resp => {
-      wx.showToast({
-        title: '取消成功',
+    var user = this.data.followings[idx]
+
+    // do cancel
+    var cancel = function() {
+      api.unfollow(user.id).then( resp => {
+        wx.showToast({
+          title: '取消成功',
+        })
+        var data = view.data.followers.splice(idx, 1)
+        view.setData({ followers: data})
+      }).catch( err => {
+        console.log(err)
+        wx.showToast({
+          title: '取消失败:'+err.code, icon: 'none'
+        })
       })
-      var data = view.data.followers.splice(idx, 1)
-      view.setData({ followers: data})
-    }).catch( err => {
-      console.log(err)
-      wx.showToast({
-        title: '取消失败:'+err.code, icon: 'none'
-      })
+    }
+
+    // show warning 
+    wx.showModal({
+      title: '提示',
+      content: `确认要取消关注: ${user.nickname} 吗？`,
+      success(res) {
+        if (res.confirm) {
+          cancel()
+        }
+      }
     })
   }, 
 
   clickFollow: function(e) {
     var idx = e.currentTarget.dataset.idx
-    var user = view.data.followers[idx]
+    var user = this.data.followers[idx]
     api.follow(user.id).then( resp => {
       wx.showToast({
         title: '关注成功',
@@ -87,37 +111,53 @@ Page({
 
 function fetchFollowerList(view) {
   view.setData({ pageNumber: 1})
+  view.setData({ loader: {ing: true, more: true}})
   api.getFollowerList(0, 1, PAGE_SIZE).then( resp => {
     console.log("get follower..", resp)
+    var hasmore = resp.data && resp.data === PAGE_SIZE
+    view.setData({ loader: {ing: false, more: hasmore}})
     view.setData({ followers: resp.data })
   }).catch( err => {
     console.log(err)
+    view.setData({ loader: { ing: true, more: true } })
   })
 }
 
 function fetchMoreFollower(view) {
   const page = view.data.pageNumber + 1
+  view.setData({ loader: { ing: true, more: view.data.loader.more } })
   api.getFollowerList(0, page, PAGE_SIZE).then( resp => {
+    var hasmore = resp.data && resp.data === PAGE_SIZE
+    view.setData({ loader: { ing: false, more: hasmore } })
     view.setData({ followers: view.data.followers.concat(resp.data)})
   }).catch( err => {
     console.log(err)
+    view.setData({ loader: { ing: false, more: view.data.loader.more} })
   })
 }
 
 function fetchFollowingList(view) {
   view.setData({ pageNumber: 1 })
+  view.setData({ loader: { ing: true, more: true } })
   api.getFollowingList(0, 1, PAGE_SIZE).then( resp => {
-    view.setData({ following: resp.data})
+    var hasmore = resp.data && resp.data === PAGE_SIZE
+    view.setData({ loader: { ing: false, more: hasmore } })
+    view.setData({ followings: resp.data})
   }).catch( err => {
     console.log(err)
+    view.setData({ loader: { ing: false, more: true } })
   })
 }
 
 function fetchMoreFollowing(view) {
   const page = view.data.pageNumber + 1
+  view.setData({ loader: { ing: true, more: view.data.loader.more } })
   api.getFollowingList(0, page, PAGE_SIZE).then(resp => {
-    view.setData({ following: view.data.followings.concat(resp.data)})
+    var hasmore = resp.data && resp.data === PAGE_SIZE
+    view.setData({ loader: { ing: false, more: hasmore } })
+    view.setData({ followings: view.data.followings.concat(resp.data)})
   }).catch( err => {
     console.log(err)
+    view.setData({ loader: { ing: false, more: view.data.loader.more } })
   })
 }
