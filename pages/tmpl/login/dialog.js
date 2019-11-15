a54c421c58d5ab19aca1fdc117b9bd9bc08ef90c
ntop <ntop.liu@gmail.com>
2019-11-15 21:14:00 +0800

    add login dialog

---
+++ pages/tmpl/login/dialog.js
@@ -0,0 +1,33 @@
+const kawa = require('../../../kawa.js')
+const app = getApp()
+
+// pages/tmpl/login/dialog.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+  
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    color: kawa.Theme.MainColor,
+    show: true,
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    clickClose: function(e) {
+
+    },
+
+    clickFinish: function(e) {
+
+    },
+  }
+})
