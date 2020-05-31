1c6d2f002cdfd6b480d5e990e1069ae19aa2efe5
ntop <ntop.liu@gmail.com>
2020-05-31 17:09:24 +0800

    setup enroll page

---
+++ pages/enroll/enroll.js
@@ -0,0 +1,31 @@
+// pages/enroll/enroll.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    theme: {
+      color: "#1890ff",
+    }, 
+    enroll: {
+      title: "汉服见面",
+      poster: "https://images.kawaapp.com/img_bnsru0kdbfdqidk66n0g.jpg",
+      detail: "大家都来参加下，限定服装汉服！"
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
